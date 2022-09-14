2415f3dffe30e44ea16fe8b5250a16d132767dfb
Harshad <hwankhede45@gmail.com>
2022-09-14 16:44:12 +0530

    Third Commit multiply file added

---
+++ Third.java
@@ -0,0 +1,12 @@
+package com.ecom.cart;
+
+public class AdditionofProduct {
+	static int a = 10;
+	static int b = 20;
+
+	public static void main(String[] args) {
+		System.out.println("Addition of product"+(a + b));
+		System.out.println("Thank you visit Again");
+		System.out.println("Thank you visit Again");
+	}
+}
